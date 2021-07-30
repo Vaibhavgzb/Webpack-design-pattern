5fcb5ecec014a0fe8cc24ef54e4f29002784e77d
Vaibhavgzb <vaibhavgupta.gzb@gmail.com>
2021-07-30 15:52:23 +0530

    Add web config file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry : {
+		main : path.resolve(__dirname, './src/app.js'),
+	},
+	output : {
+		filename : "[name].bundle.js",
+		path : path.resolve(__dirname, 'deploy')
+	},
+	module : {
+		rules : [
+			{
+				test : /\.css$/,
+				use : ["style-loader", "css-loader"]
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title : "Webpack OUTPUT",
+		}),
+	],
+};
